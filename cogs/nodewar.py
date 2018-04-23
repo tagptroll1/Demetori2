f26b3f2c52b4c03b393341a4914c05e26a12669a
tagp <thomas@galehuset.org>
2018-04-23 13:31:08 +0200

    Commented nodewar.py

--- cogs/nodewar.py
+++ cogs/nodewar.py
@@ -29,6 +29,10 @@ class Nodewar:
         
     @commands.command()
     async def nodewars(self, ctx):
+        """Creates a blank nodewar embed with teamspeak, and few other presets
+        
+        This embed is fully editable"""
+
         await ctx.message.delete()
         nw_box = CustomizableNodewar(self.bot, ctx)
         self.stored_embeds[ctx.channel.id] = nw_box
@@ -36,16 +40,24 @@ class Nodewar:
 
     @commands.command()
     async def set_embed(self, ctx, id:int):
+        """Sets which embed is in focus for editing, and other checks"""
+        
         await ctx.message.delete()
         self.stored_embeds[ctx.channel.id] = await ctx.channel.get_message(id)
 
 ##------------------EDIT-----
     @commands.group(invoke_without_command=True)
     async def edit(self, ctx):
+        """Parent command for editing embed"""
         pass
 
     @edit.command(name="url")
     async def edit_url(self, ctx, *, url):
+        """Sub-Command for editing url of embed, url applies to thumbnail and title
+        
+        ?edit url <url>
+        url: Sets urls in embed to this, silently ignores invalid urls"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -58,6 +70,11 @@ class Nodewar:
 
     @edit.command(name="title")
     async def edit_title(self, ctx, *, title):
+        """Sub-Command for editing title of embed
+        
+        ?edit title <title>
+        title: Sets title to this"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -67,6 +84,11 @@ class Nodewar:
 
     @edit.command(name="description")
     async def edit_description(self, ctx, *, desc):
+        """Sub-Command for editing description in embed
+        
+        ?edit description <text>
+        text: Changed embed description to this"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -76,6 +98,11 @@ class Nodewar:
 
     @edit.command(name="thumbnail")
     async def edit_thumbnail(self, ctx, *, url):
+        """Sub-Command for editing thumbnail in embed
+        
+        ?edit thumbnail <url>
+        url: A valid url of an image, silently excepts invalid urls"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -85,6 +112,11 @@ class Nodewar:
 
     @edit.command(name="image")
     async def edit_image(self, ctx, *, url):
+        """Sub-Command for editing image in embed
+
+        ?edit image <url>
+        url: A valid url of an image, silently excepts invalid urls"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -94,6 +126,11 @@ class Nodewar:
 
     @edit.command(name="author")
     async def edit_author(self, ctx, *, authorname):
+        """Sub-Command for editing author in embed
+        
+        ?edit author <author>
+        author: Name of an author to add"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -104,10 +141,15 @@ class Nodewar:
 ##-------------------TOGGLE----------
     @commands.group(invoke_without_command=True)
     async def toggle(self, ctx):
+        """Parent command for toggling in embed"""
         pass
 
     @toggle.command(name="timestamp")
     async def toggle_timestamp(self, ctx):
+        """Sub-Command for toggeling timestamps
+        
+        ?toggle timestamp"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -118,10 +160,17 @@ class Nodewar:
 ##---------------------ADD-----------
     @commands.group(invoke_without_command=True)
     async def add(self, ctx):
+        """Parent command for adding to embed"""
         pass
 
     @add.command(name="field")
     async def add_field(self, ctx, title, *, field):
+        """Sub-Command for adding a field to embed
+
+        ?add field <title> <text>
+        title: The title of the field added
+        text : The text appearing under the title"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -132,10 +181,16 @@ class Nodewar:
 ##-------------REMOVE-----------
     @commands.group(invoke_without_command=True)
     async def remove(self, ctx):
+        """Parent command for removing in embed"""
         pass
 
     @remove.command(name="field")    
     async def remove_field(self, ctx, index:int):
+        """Sub-Command for deleting a field in embed
+        
+        ?remove field <id>
+        id: index of field to be removed starting at 1 from upper left"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -146,10 +201,15 @@ class Nodewar:
 ##--------------CLEAR-----------
     @commands.group(invoke_without_command=True)
     async def clear(self, ctx):
+        """Parent command for clearing in embed"""
         pass
 
     @clear.command(name="field")
     async def clear_fields(self, ctx):
+        """Sub-Command for clearing all fields in embed
+        
+        ?clear field"""
+
         await ctx.message.delete()
         embed = self.has_embed(ctx.channel.id)
         if embed:
@@ -159,10 +219,15 @@ class Nodewar:
 
     @commands.group(invoke_without_command=True)
     async def preset(self, ctx):
+        """Parent command for creating pre-made nodewar embeds"""
         pass
 
     @preset.command()
     async def mandatory(self, ctx):
+        """Sub-Command for creating a preset embed for mandatory NW
+        
+        ?preset mandatory"""
+
         param = {
                 "title": "Nodewars information",
                 "description": f"{ctx.guild.name}'s mandatory nodewar information",
@@ -183,6 +248,10 @@ class Nodewar:
 
     @preset.command()
     async def optional(self, ctx):
+        """Sub-Command for creating a preset optional NW embed
+        
+        ?preset optional"""
+        
         param = {
                 "title": "Nodewars information",
                 "description": f"{ctx.guild.name}'s optional nodewar information",
